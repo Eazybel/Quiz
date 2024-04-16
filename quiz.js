8b7a25108e700f64b15ed573356000d326204dfb
Eazybel <eazybel27@gmail.com>
2024-04-16 23:14:43 +0300

    starteds

---
+++ quiz.js
@@ -0,0 +1,22 @@
+const link1 = document.getElementById("link1");
+const link2 = document.getElementById("link2");
+const btn = document.getElementById("btn");
+let para = document.getElementById("para");
+
+const fun=function(){
+   para.textContent=("This service is not available");
+ 
+} 
+const fun2=function (){
+   para.textContent=("This service is not available");
+}
+link1.onclick=setTimeout(fun,3000);
+link1.onclick=setTimeout(fun,3000);
+
+
+
+
+
+
+
+
