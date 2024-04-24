158e4c496fb626e83110819d31232c2e7772d1e0
Eazybel <eazybel27@gmail.com>
2024-04-24 17:11:31 +0300

    simple feature added

---
+++ geography.js
@@ -0,0 +1,7 @@
+const prev=document.getElementById("prev");
+const next=document.getElementById("next");
+const body=document.querySelector("body")
+const Q=document.getElementById("Q")
+prev.onclick=function(){
+    body.document.body=("#Q10")
+}
