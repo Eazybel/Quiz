632b2798369e2545f8e4e0dda2c5ac848787792e
Eazybel <eazybel27@gmail.com>
2024-04-24 18:02:04 +0300

    This commit includes changes to the geography.css file to add styling for the quiz, as well as updates to the geography.html file

--- geography.js
+++ geography.js
@@ -2,6 +2,17 @@ const prev=document.getElementById("prev");
 const next=document.getElementById("next");
 const body=document.querySelector("body")
 const Q=document.getElementById("Q")
-prev.onclick=function(){
-    body.document.body=("#Q10")
+const a=document.getElementById("a")
+const train=document.getElementById("train")
+var answer=0
+next.onclick=function(){
+    switch (a.checked) {
+        case true:
+            train.textContent="correct"
+            break;
+        case false:
+            train.textContent="incorrect"
+        default:
+            break;
+    }
 }
