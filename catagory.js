e406f716970490be54145916f5b98a136d0c99fe
Eazybel <eazybel27@gmail.com>
2024-07-15 18:08:25 +0300

    additional bug fixed

--- catagory.js
+++ catagory.js
@@ -1,5 +1,40 @@
 const btnd = document.getElementById("btnd");
 const body = document.querySelector("body")
+const geography=document.getElementById("geography")
+const history=document.getElementById("history")
+const science=document.getElementById("science")
+const literature=document.getElementById("literature")
+const art=document.getElementById("art")
+const sports=document.getElementById("sports")
+const brands=document.getElementById("brands")
+const football=document.getElementById("football")
+const famous=document.getElementById("famous")
 btnd.onclick=function(){
-    btnd.onclick=document.location="geography.html"
+    if (geography.checked) {
+        document.location=("geography.html")
+    }
+   if (history.checked) {
+        document.location=("history.html")
+    }
+    if (science.checked) {
+        document.location=("science.html")
+    }
+    if (literature.checked) {
+        document.location=("literature.html")
+    }
+    if (art.checked) {
+        document.location=("art.html")
+    }
+    if (sports.checked) {
+        document.location=("sports.html")
+    }
+    if (brands.checked) {
+        document.location=("brands.html")
+    }
+    if (football.checked) {
+        document.location=("football.html")
+    }
+    if (famous.checked) {
+        document.location=("famous.html")
+    }
 }
