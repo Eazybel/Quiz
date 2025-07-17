20ddf75336fe271a5a65844f8e57565b48c3d5e2
Eazybel <eazybel27@gmail.com>
2025-07-17 23:04:20 +0300

    Authentication implemented sucessfully and added indipendant pages for each

--- register.js
+++ register.js
@@ -1,7 +1,7 @@
 
 // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
-  import { getAuth,  signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
+  import { getAuth, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
   
@@ -20,15 +20,22 @@ const app = initializeApp(firebaseConfig);
 const auth=getAuth(app)
 auth.languageCode="en"
 const btn=document.getElementById("btn")
+const hider=document.getElementById("hider")
+const wrapper=document.getElementById("wrapper")
 btn.onclick=(e)=>{
     e.preventDefault()
       const email=document.getElementById("email")
       const password=document.getElementById("password")
-       signInWithEmailAndPassword(auth, email.value, password.value)
+      createUserWithEmailAndPassword(auth, email.value, password.value)
   .then((userCredential) => {
     // Signed up 
     const user = userCredential.user;
-        window.location="./profile.html"
+    const header=document.getElementById("header")
+    header.innerHTML=`Account created successfully`
+    header.style.color="green"
+btn.style.display="none"
+hider.style.display="block"
+wrapper.style.display="none"
     // ...
   })
   .catch((error) => {
@@ -36,11 +43,14 @@ btn.onclick=(e)=>{
     const errorMessage = error.message;
     // ..
   });
-
   }
- const provider=new GoogleAuthProvider()
+  hider.onclick=()=>{
+    window.location="./index.html"
+  }
+  const provider=new GoogleAuthProvider()
+  const google=document.getElementById("google")
   const google1=document.getElementById("google1")
-  google1.onclick=()=>{
+  google.onclick=()=>{
     signInWithPopup(auth, provider)
   .then((result) => {
     const credential = GoogleAuthProvider.credentialFromResult(result);
@@ -54,3 +64,4 @@ btn.onclick=(e)=>{
     const credential = GoogleAuthProvider.credentialFromError(error);
   });
   }
+ 
